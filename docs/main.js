1642f29f79374c5c88f7cbd6e272ddbf3e9e96ff
Stitsheo <166002323+AllanVitu@users.noreply.github.com>
2024-05-23 16:00:30 +0200

    version 1.0 mobile

---
+++ docs/main.js
@@ -0,0 +1,7 @@
+const btnMenu = document.getElementById('btnMenu');
+const menu = document.getElementById('menu');
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
