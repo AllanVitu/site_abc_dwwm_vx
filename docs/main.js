dc1490c67242a10d2ec1ef3c14cec11bf89006cb
Stitsheo <166002323+AllanVitu@users.noreply.github.com>
2024-05-30 13:39:13 +0200

    V 1.4

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,6 @@
 const btnMenu = document.getElementById('btnMenu');
 const menu = document.getElementById('menu');
 
-// Add a class for better styling control
-menu.classList.add('hidden'); // Assuming the hidden class hides the menu
-
 btnMenu.addEventListener('click', () => {
-  menu.classList.toggle('hidden'); // Toggle between hidden and visible states
+    menu.classList.toggle('active');
 });
-
